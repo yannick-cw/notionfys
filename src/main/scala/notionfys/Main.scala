f01249673881608b7830ca01439f489cd495463c
ygladow <G>
2020-04-25 14:48:33 +0200

    Improve error reporting

--- src/main/scala/notionfys/Main.scala
+++ src/main/scala/notionfys/Main.scala
@@ -22,11 +22,22 @@ object Main
   implicit val H: Highlights[AppM] = Highlights[AppM]
   implicit val C: Console[AppM]    = Console
 
-  private def errorMsg(err: String) =
-    s"Unfortunately syncing failed, you can run with --verbose to see more details about what is going on. Please report the output to https://github.com/yannick-cw/notionfys, so I can fix it. Here is the raw error:\n$err"
+  private def errorMsg(err: Throwable, args: Args) =
+    s"Unfortunately syncing failed, you can run with --verbose to see more details about what is going on. Please report the output to https://github.com/yannick-cw/notionfys, so I can fix it. Here is the raw error:\n${if (args.verbose)
+      err.getLocalizedMessage ++ "\n" ++ err.getStackTrace.mkString("\n")
+    else err.getMessage}"
 
   override def main: Opts[IO[ExitCode]] =
     Cli.parseArgs
-      .map(Program.updateNotion[AppM].run(_).as(println("Welcome")).as(ExitCode.Success))
-      .map(_.onError { case NonFatal(err) => IO(println(errorMsg(err.toString))) })
+      .map(
+        args =>
+          Program
+            .updateNotion[AppM]
+            .run(args)
+            .as(println("Welcome"))
+            .as(ExitCode.Success)
+            .recoverWith {
+              case NonFatal(err) => IO(println(errorMsg(err, args))).as(ExitCode.Error)
+            }
+      )
 }
--- src/main/scala/notionfys/Program.scala
+++ src/main/scala/notionfys/Program.scala
@@ -14,20 +14,20 @@ object Program {
   ): F[Unit] =
     for {
       kindlePath        <- Ask.reader(_.kindle)
-      _                 <- Console.log(s"Reading highlights from Kindle at $kindlePath....")
+      _                 <- Console.verboseLog(s"Reading highlights from Kindle at $kindlePath....")
       kindleFile        <- FS.readF(kindlePath / "documents" / "My Clippings.txt")
-      _                 <- Console.log("Done readingkindhle highlights")
+      _                 <- Console.verboseLog("Done readingkindhle highlights")
       kindleHighlights  <- Highlights.parseKindleHighlights(kindleFile)
-      _                 <- Console.log("Parsed kindle highlights to internal format")
-      _                 <- Console.log("Fetching highlights from Notion...")
+      _                 <- Console.verboseLog("Parsed kindle highlights to internal format")
+      _                 <- Console.verboseLog("Fetching highlights from Notion...")
       currentHighlights <- Notion.getSubPages
-      _                 <- Console.log("Fetched current highlghts from Notion")
-      _                 <- Console.log(s"\nFound Highlights:\n${currentHighlights.map(_.title).mkString("\n")}\n")
+      _                 <- Console.verboseLog("Fetched current highlghts from Notion")
+      _                 <- Console.verboseLog(s"\nFound Highlights:\n${currentHighlights.map(_.title).mkString("\n")}\n")
       newHighlights = kindleHighlights.filterNot(
         h => currentHighlights.exists(cH => cH.title == h.title && cH.content == h.content)
       )
-      _ <- Console.log(s"\nSyncing new highlights:\n${newHighlights.map(_.title).mkString("\n")}\n")
-      _ <- Console.log("....")
+      _ <- Console.verboseLog(s"\nSyncing new highlights:\n${newHighlights.map(_.title).mkString("\n")}\n")
+      _ <- Console.verboseLog("....")
       _ <- newHighlights.traverse(Notion.addSubPage)
       _ <- Console.log("Done syncing new highlights to Notion")
       _ <- Console.log("Shutting down")
